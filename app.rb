d46eef9ab32fee21d425a7b10cb0047c46f318c6
tera343 <tera343@gmail.com>
2015-07-12 21:04:57 +0900

    郵便番号の新規追加まで実装

--- app.rb
+++ app.rb
@@ -9,8 +9,23 @@ end
 
 get '/' do
 	sql = <<-SQL
-		SELECT * FROM ziplist
+		SELECT * FROM ziplist;
 	SQL
 	@rows = @db.execute(sql)
 	erb :index,layout: :layout
+end
+
+get '/new' do
+	erb :new,layout: :layout
+end
+
+post '/new/receive' do
+	@zipcode = params[:zipcode]
+	@address = params[:address]
+	sql = <<-SQL
+		INSERT INTO ziplist(zipcode,address)
+		VALUES("#{@zipcode}","#{@address}");
+	SQL
+	@db.execute(sql)
+	redirect '/'
 end
