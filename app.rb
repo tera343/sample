b77342c288d542c0886ca7abfef79b27ec13080c
tera343 <tera343@gmail.com>
2015-07-12 20:12:09 +0900

    郵便番号の一覧表示まで

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
 require 'sqlite3'
+before do
+	@db = SQLite3::Database.new("ziplist.db")
+end
+after do
+	@db.close
+end
 
 get '/' do
-	
+	sql = <<-SQL
+		SELECT * FROM ziplist
+	SQL
+	@rows = @db.execute(sql)
 	erb :index,layout: :layout
 end
